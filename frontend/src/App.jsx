e8252dee37e195f272469b9741c38fffa847e73c
SohailDAD <dsohail866@gmail.com>
2025-06-05 22:47:46 +0500

    create the components and pages folder and files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import { Button } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import CreatePage from "./pages/createPage";
 
 function App() {
 	return (
-		<>
-			<h1>Hello World!</h1>
-			<Button>hi</Button>
-		</>
+		<Box minH={"100vh"}>
+			<Routes>
+				<Route path="/" element={<HomePage/>} />
+				<Route path="/create" element={<CreatePage/>} />
+			</Routes>
+		</Box>
 	);
 }
 
