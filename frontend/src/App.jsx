bda8a5d610205ba15d07684addb88f3d47738f4b
SohailDAD <dsohail866@gmail.com>
2025-06-05 23:22:00 +0500

    install the react icon pakage and use in Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/homePage";
-import CreatePage from "./pages/createPage";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<Box minH={"100vh"}>
+			<Navbar />
 			<Routes>
-				<Route path="/" element={<HomePage/>} />
-				<Route path="/create" element={<CreatePage/>} />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/create" element={<CreatePage />} />
 			</Routes>
 		</Box>
 	);
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { Button, Container, Flex, HStack, Text } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { CiSquarePlus } from "react-icons/ci";
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <Container maxW={"1140px"} px={4} >
+        <Flex
+        h={16}
+        align={'center'}
+        justify={'space-between'}
+        flexDir={
+            { base: 'column', sm: 'row' }
+        }
+        >
+        
+        <Text
+        fontSize={{base: '22', sm: '28'}}
+        fontWeight={'bold'}
+        textTransform={'uppercase'}
+        textAlign={'center'}
+        bgGradient={'linear(to-r, cyan.400, blue.500)'}
+        bgClip={'text'}
+        >
+            <Link to='/'> Product Store  🛒 </Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to='/create'>
+            <Button>
+              <CiSquarePlus fontSize={20}/>
+            </Button>
+          </Link>
+        </HStack>
+
+        </Flex>
+    </Container>
   )
 }
 
